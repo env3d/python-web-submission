90686629097a78cdf36df164549e0d1d2bc93151
env3d <jmadar@gmail.com>
2023-10-18 10:08:56 -0700

    Hacked together skulpt runtime for turtle. Added basic console

---
+++ skulpt-run.js
@@ -0,0 +1,70 @@
+// Taken and modified from https://skulpt.org/using.html#html
+
+// output functions are configurable.  This one just appends some text
+// to a pre element.
+function outf(text) {
+  text = text.trim();
+  if (text.length > 0) {
+    term.write('\r\n');
+    term.write(text);
+  }
+
+  //var mypre = document.getElementById("console"); 
+  //mypre.innerHTML = mypre.innerHTML + text; 
+} 
+function builtinRead(x) {
+  if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+    throw "File not found: '" + x + "'";
+  return Sk.builtinFiles["files"][x];
+}
+
+function initSkulpt(){
+  //Sk.pre = "console";
+  
+
+  console.log('init skulpt');
+  Sk.configure( {
+    output: outf,
+    read: builtinRead,
+    retainGlobals: true,
+    __future__: Sk.python3
+  });
+
+  Sk.globals = { __name__: new Sk.builtin.str("__main__")};
+  
+  (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'output';
+  (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).width = 0;
+  (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).height = 0;
+
+
+}
+
+// Here's everything you need to run a python program in skulpt
+// grab the code from your textarea
+// get a reference to your pre element for output
+// configure the output function
+// call Sk.importMainWithBody()
+function runSkulpt(prog) {
+  var prog = editor.session.getValue();
+  //var mypre = document.getElementById("console"); 
+  //mypre.innerHTML = '';
+
+  myPromise = Sk.misceval.asyncToPromise(function() {    
+    return Sk.importMainWithBody("<stdin>", false, prog, true);
+  });
+  myPromise.then(function(mod) {
+    console.log('success');
+  }, function(err) {
+    console.log(err.toString());
+  });
+  /*
+  var myPromise = Sk.misceval.asyncToPromise(function() {
+    return Sk.importMainWithBody("<stdin>", false, prog, true);
+  });
+  myPromise.then(function(mod) {
+    console.log('success');
+  }, function(err) {
+    console.log(err.toString());
+  });
+  */
+} 
